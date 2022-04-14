f2d968391f7bd79d2d6b2d76ede081a690eed378
verve-neowise <96300181+verve-neowise@users.noreply.github.com>
2022-04-14 09:13:45 +0500

    Add files via upload

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(9090, () => console.log('Server is running on http://localhost:9090'))
